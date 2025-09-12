1b11bb8d900bdba97fcb9615e4b0d03f86a0dfd7
akbarzadehmojtabasaghaei-del <akbarzadeh.mojtaba.saghaei@gmail.com>
2025-09-12 23:24:45 +0330

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -3,3 +3,11 @@ print("I am now working from VS Code!") # این خط جدید است
 x = 2
 print(x)
 print(x + 2)
+x = 5
+y = 9
+z = 12 
+print(x + y)
+print(x + y + z +2)
+print(type(x))
+# hi
+# print(type(hi))
